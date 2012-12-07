3ad74e2150a6206ddee774305875f30f6538a882
Andrés Moreira <andres@andresmoreira.com>
2012-12-07 11:28:04 -0200

    make seenpages an instance attribute

--- scifibot/middleware.py
+++ scifibot/middleware.py
@@ -7,7 +7,8 @@ class DuplicateDetection(object):
     """
     Simple duplicate detection middleware
     """
-    seenpages = set()
+    def __init__(self):
+        self.seenpages = set()
 
     def process_response(self, request, response, spider):
         fp = fingerprint(response.body)
